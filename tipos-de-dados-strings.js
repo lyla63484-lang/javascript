92084b1755be9988c6e820ee43d5fd2049ca7589
lavinia-barbosa <lyla63484@gmail.com>
2026-09-11 09:18:50 -0300

    tipos-de-dados-strings.js

---
+++ tipos-de-dados-strings.js
@@ -0,0 +1,7 @@
+let nome = "Ana";
+let sobrenome = 'Silva';
+let saudacao = `Olá, ${nome}!`;
+
+console.log("Nome: ", nome)
+console.log("Sobrenome: ", sobrenome)
+console.log("Saudacao: ", saudacao)
