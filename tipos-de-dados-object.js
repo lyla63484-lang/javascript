51e97a02db42156edf2a402dc9043b797f493dc7
lavinia-barbosa <lyla63484@gmail.com>
2026-09-11 11:02:29 -0300

    tipos-de-dados-object.js

--- tipos-de-dados-object.js
+++ tipos-de-dados-object.js
@@ -6,3 +6,11 @@ let pessoa = {
 // Acessando propriedades do objeto
 console.log(pessoa.nome); // "Carlos"
 console.log(pessoa.idade); // 28
+console.log("Casado:", pessoa.casado);
+console.log("Tipo de pessoa:", typeof pessoa);
+console.log("Propriedades do objeto pessoa:", Object.keys(pessoa));
+console.log("Valores do objeto pessoa:", Object.values(pessoa));
+console.log("Objeto pessoa completo:", pessoa);
+console.log("Objeto pessoa em formato JSON:", JSON.stringify(pessoa));
+console.log("Objeto pessoa em formato JSON (com indentação):", JSON.stringify(pessoa, null, 2));
+console.log("Objeto pessoa em formato JSON (com indentação e espaçamento):", JSON.stringify(pessoa, null, 4));
