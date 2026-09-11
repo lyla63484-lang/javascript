1a11127d4e6bef3b730bb040c80e12b0b3e2a0d8
lavinia-barbosa <lyla63484@gmail.com>
2026-09-11 10:51:41 -0300

    tipos-de-dados-array.js

---
+++ tipos-de-dados-array.js
@@ -0,0 +1,15 @@
+let frutas = ["maça", "banana", "laranja"]; // Array de strings
+let numeros = [1, 2, 3, 4, 5,]; // Array de numeros
+let misto = [true,"text", 42]; // Array com diferentes tipos de dados
+
+console.log("Frutas:", frutas);
+console.log("Números:", numeros);
+console.log("Misto:", misto);
+console.log("Tipo de frutas: ", typeof frutas)
+console.log("Tipo de numeros: ", typeof numeros)
+console.log("Tipo de misturados: ", typeof misturado)
+console.log("Primeira fruta: ", frutas[0]);
+console.log("Última fruta:", frutas[frutas.length - 1]);
+console.log("Soma dos números:", numeros[0] + numeros[1]);
+console.log("Concatenando arrays:", frutas.concat(numeros));
+console.log("Array misturado:", misturado);
