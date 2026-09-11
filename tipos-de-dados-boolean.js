abe4f804a0a0de80a0a5fffa0c2ca273968e48ce
lavinia-barbosa <lyla63484@gmail.com>
2026-09-11 09:17:48 -0300

    tipos-de-dados-boolean.js

---
+++ tipos-de-dados-boolean.js
@@ -0,0 +1,7 @@
+let aprovado = true;
+let reprovado = false;
+
+console.log("Aprovado:", aprovado);
+console.log("Reprovado:", reprovado);
+console.log("Tipo de aprovado:", typeof aprovado);
+console.log("Tipo de reprovado:", typeof reprovado);
